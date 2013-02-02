ace787b5f30c404dac5dd64f1970203c054d0af8
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-02-01 17:13:21 -0800

    Some styling for friends page.  Also added in Cakefile bugfix from apptemplateadv

--- friends/templates/main.php
+++ friends/templates/main.php
@@ -1,16 +1,33 @@
 <div id="app" ng-app="Friends"
-	ng-init="name='{{somesetting}}'">
+	ng-init="tab=0;">
 
 	<h1 class="heading">Friends</h1>
 
-	<!--<p>The URL Parameter for the index page is: {{test}}</p>-->
 
-	<p ng-show="name">Welcome home [[name | leetIt]]!</p>
 
+	<div class="tabnav">	
+		<a href="#" ng-click="tab=0" class="selectedTab-[[tab==0]]">Your Friends</a>
+		<a href="#" ng-click="tab=1" class="selectedTab-[[tab==1]]">Add Friends</a>
+		<a href="#" ng-click="tab=2" class="selectedTab-[[tab==2]]">Accept Friend Requests</a>
+	</div>
+		
 	
+	<div class="app-body">	
+	<!-- friends -->
+	<div ng-show="tab==0" ng-controller="FriendshipController">
+			<span ng-repeat="friendship in friendships">
+				[[friendship]]
+			</span>
+			<p ng-hide="friendships">
+				You do not have any friends.
+			</p>
+		
+	</div>
 
-	<div ng-controller="FRController">
-		<div>
+
+
+	<div  ng-controller="FRController">
+		<div ng-show="tab==1" class="">
 			<form class="centered">
 				<input type="text" placeholder="Enter username" ng-model="recipient">
 				<button ng-click="createFriendshipRequest(recipient)">Request Friendship</button>
@@ -20,27 +37,22 @@
 			<span ng-repeat="friendshipRequest in sentFriendshipRequests">
 				[[friendshipRequest]]
 			</span>
+			<p ng-hide="sentFriendshipRequests">
+				You are not waiting on any friend requests.
+			</p>
 		</div>
 
 		<!--received requests -->
-		<div>
+		<div ng-show="tab==2" class="">
 			<span ng-repeat="friendshipRequest in receivedFriendshipRequests">
 				[[friendshipRequest]]
 			</span>
+			<p ng-hide="receivedFriendshipRequests">
+				You have responded to all your friend requests.
+			</p>
 		</div>
 	</div>
 
-		
-	
-	
-	<!-- friends -->
-	<div ng-controller="FriendshipController">
-		<br /><br />
-		<p>Your friends are: </p>
-			<span ng-repeat="friendship in friendships">
-				[[friendship]]
-			</span>
-		
 	</div>
 </div>
 
