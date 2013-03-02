6e19691e2dac4304195d7633cac5a540256c06c3
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-03-01 17:20:28 -0800

    rsync file to run from cron

---
+++ multiinstance/cron/rsync.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ownCloud - Multi Instance
+ *
+ * @author Sarah Jones
+ * @copyright 2013 Sarah Jones sarah.e.p.jones@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\MultiInstance\Cron;
+
+#rsync only with UCSB server
+exec("rsync --verbose --compress  \
+      --recursive --times --perms --links --delete \
+      --exclude "*~" \
+      db_sync/ www-data@192.168.56.101::db_sync_recv/village1");
+
