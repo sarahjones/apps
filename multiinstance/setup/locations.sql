e9a4faef589e7046cd21a5ba2d884d3c609e141b
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-04-09 14:13:29 -0700

    Update setup/README after another test run.

---
+++ multiinstance/setup/locations.sql
@@ -0,0 +1,5 @@
+INSERT INTO table_name oc_multiinstance_locations VALUES ("UCSB", "192.168.56.101");
+INSERT INTO table_name oc_multiinstance_locations VALUES ("Kalene", "192.168.56.103");
+INSERT INTO table_name oc_multiinstance_locations VALUES ("Macha", "192.168.56.102");
+
+
