60ebccc31874d0449df2593d512015175b27b418
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-04-09 14:17:34 -0700

    Typo fix

--- multiinstance/setup/locations.sql
+++ multiinstance/setup/locations.sql
@@ -1,5 +1,5 @@
-INSERT INTO table_name oc_multiinstance_locations (location, ip) VALUES ("UCSB", "192.168.56.101");
-INSERT INTO table_name oc_multiinstance_locations (location, ip) VALUES ("Kalene", "192.168.56.103");
-INSERT INTO table_name oc_multiinstance_locations (location, ip) VALUES ("Macha", "192.168.56.102");
+INSERT INTO oc_multiinstance_locations (location, ip) VALUES ("UCSB", "192.168.56.101");
+INSERT INTO oc_multiinstance_locations (location, ip) VALUES ("Kalene", "192.168.56.103");
+INSERT INTO oc_multiinstance_locations (location, ip) VALUES ("Macha", "192.168.56.102");
 
 
