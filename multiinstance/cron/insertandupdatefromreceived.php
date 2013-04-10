cead6722e3a72508786f2956c04650601b32cdc5
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-04-10 12:55:34 -0700

    insertandupdatefromreceived.php got messed up with cp

--- multiinstance/cron/insertandupdatefromreceived.php
+++ multiinstance/cron/insertandupdatefromreceived.php
@@ -1,30 +1,38 @@
-#!/bin/sh
-###
-# ownCloud - Multi Instance
-#
-# @author Sarah Jones
-# @copyright 2013 Sarah Jones sarah.e.p.jones@gmail.com
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
-# License as published by the Free Software Foundation; either
-# version 3 of the License, or any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU AFFERO GENERAL PUBLIC LICENSE for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-#
-###
+<?php
+/**
+ * ownCloud - Multi Instance
+ *
+ * @author Sarah Jones
+ * @copyright 2013 Sarah Jones sarah.e.p.jones@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\MultiInstance\Cron;
 
 
-if ps -ef | grep -v grep | grep updatefromreceived.php ; then
-        exit 0
-else
-	#Change this path to be the path to multiinstance/cron/updatefromreceived.php
-        /home/sarah/public_html/apps/multiinstance/cron/updatefromreceived.php >> /home/sarah/public_html/apps/multiinstance/cron/error.txt &
-        exit 0
-fi
+require_once(__DIR__ . "/../tests/classloader.php");
+require_once(__DIR__ . "/../../../owncloud/lib/public/config.php");
+require_once(__DIR__ . "/../../../owncloud/lib/config.php");
+
+use OCA\MultiInstance\Lib\CronHelper;
+use OCA\MultiInstance\DependencyInjection\DIContainer;
+
+
+$c = new DIContainer();
+$c['CronTask']->insertReceived();
+$c['CronTask']->updateUsersWithReceivedUsers();
+$c['CronTask']->updateFriendshipsWithReceivedFriendships();
+$c['CronTask']->updateUserFacebookIdsWithReceivedUserFacebookIds();
